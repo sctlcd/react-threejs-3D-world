9a5e19c73f20d6350416757a919cc97bde5a29ed
sctlcd <lucinda.souchet@gmail.com>
2022-10-05 19:19:46 +0200

    add props to Box mesh component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
+// a React renderer for Three.js
 import { Canvas } from '@react-three/fiber';
 // useful helpers for react-three-fiber
-import { OrbitControls, Stars, PerspectiveCamera } from "@react-three/drei";
-import './App.css';
-// physics library "cannon" - physics based hooks
+import { OrbitControls, Stars, PerspectiveCamera } from '@react-three/drei';
+// physics library 'cannon' - physics based hooks
 import { Physics, useBox, usePlane } from '@react-three/cannon';
+
+import './App.css';
 
 
 function Box() {
   // use canon hook
   {/*
-      useBox is physics-only, it comes from the physics library "cannon".
+      useBox is physics-only, it comes from the physics library 'cannon'.
       It approximates a shape and links it to arbitrary geometry. The physics
       engine now manages a literal box, a cube, and subjects it to gravity and other.
       Pick a shape that suits your objects contact surface, it could be a box, plane, sphere, etc.
@@ -19,9 +22,13 @@ function Box() {
   // get a ref to the physics box representation - use useBox() then give it a callback, from this callback
   // we return an object that has props: position, mass (it will now be affected by gravity)
    const [ref, api] = useBox(() => ({ 
-    mass: 2, 
+    mass: 1, 
     position: [0, 12, 0]
   }));
+
+  // Hold state for clicked and hovered
+  const [clicked, click] = useState(false);
+  const [hovered, hover] = useState(false);
 
   return (
     // define mesh - equivalent to new THREE.Mesh()
@@ -30,12 +37,16 @@ function Box() {
     <mesh 
       ref={ref}
       position={[0, 12, 0]}
-      onClick={() => {
-        api.velocity.set(0, 6, 0);
+      scale={clicked ? 3 : 1}
+      onClick={(e) => {
+        api.velocity.set(0, 10, 0);
       }}
+      onContextMenu={(e) => click(!clicked)}
+      onPointerOver={(e) => hover(true)}
+      onPointerOut={(e) => hover(false)}
     > 
-      <boxBufferGeometry attach="geometry" /> // define boxBufferGeometry
-      <meshLambertMaterial attach="material" color="darkred" /> // define meshLambertMaterial
+      <boxBufferGeometry attach='geometry' /> // define boxBufferGeometry
+      <meshLambertMaterial attach='material' color={ hovered ? 'darkblue' : 'darkred'} /> // define meshLambertMaterial
     </mesh>
   );
 }
@@ -56,8 +67,8 @@ function Plane() {
       ref={ref}
       rotation={[-Math.PI / 2, 0, 0]}
     >
-      <planeBufferGeometry attach="geometry" args={[100, 100]}/> // define planeBufferGeometry
-      <meshLambertMaterial attach="material" color="grey" /> // define meshLambertMaterial
+      <planeBufferGeometry attach='geometry' args={[100, 100]}/> // define planeBufferGeometry
+      <meshLambertMaterial attach='material' color='grey' /> // define meshLambertMaterial
     </mesh>
   );
 }
