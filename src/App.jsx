5bc3ff95903055d0053e23b0bb75bb81595bdc4b
sctlcd <lucinda.souchet@gmail.com>
2022-10-05 09:33:47 +0200

    add stars background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import './App.css'
 
 
@@ -19,6 +19,7 @@ function App() {
     <Canvas>
       {/* Orbit controls allow the camera to orbit around a target. */}
       <OrbitControls /> // define OrbitControls
+      <Stars /> // define Stars
       <ambientLight intensity={0.5} /> // define ambientLight
       <spotLight position={[10, 15, 10]} angle={0.3} /> // define spotLight with position and angle props
       <Box /> // define Box
