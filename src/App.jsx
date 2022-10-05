19760568b2258130858c6c4bf0b13fa9d3c79be8
sctlcd <lucinda.souchet@gmail.com>
2022-10-05 15:14:28 +0200

    add Physics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from "@react-three/drei";
 import './App.css'
+import { Physics, useBox, usePlane } from '@react-three/cannon';
 
 
 function Box() {
+  // use canon hook
+  const [ref] = useBox(() => ({ 
+    mass: 1, 
+    position: [0, 6, 0] 
+  }));
+
   return (
-    <mesh position={[-2, 1, 2]}> // define mesh - equivalent to new THREE.Mesh() - with position prop
+    // define mesh - equivalent to new THREE.Mesh()
+    <mesh 
+      ref={ref}
+      position={[0, 6, 0]}
+    > 
       <boxBufferGeometry attach="geometry" /> // define boxBufferGeometry
       <meshLambertMaterial attach="material" color="darkred" /> // define meshLambertMaterial
     </mesh>
@@ -13,15 +24,24 @@ function Box() {
 }
 
 function Plane() {
+  // use canon hook
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0]
+  }));
+
   return (
-    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}> // define mesh with position and rotation props
+     // define mesh
+    <mesh 
+      ref={ref}
+      rotation={[-Math.PI / 2, 0, 0]}
+    >
       <planeBufferGeometry attach="geometry" args={[100, 100]}/> // define planeBufferGeometry
-      <meshLambertMaterial attach="material" color="darkgrey" /> // define meshLambertMaterial
+      <meshLambertMaterial attach="material" color="grey" /> // define meshLambertMaterial
     </mesh>
   );
 }
 
-function App() {
+function App(props) {
   return (
     // Canvas component: sets up a Scene and a Camera (basic building blocks necessary for rendering)
     // and renders our scene every frame (no need for traditional render-loop)
@@ -31,8 +51,10 @@ function App() {
       <Stars /> // define Stars
       <ambientLight intensity={0.5} /> // define ambientLight
       <spotLight position={[10, 15, 10]} angle={0.3} /> // define spotLight with position and angle props
-      <Box /> // define 
-      <Plane />
+      <Physics> // create a physics world
+        <Plane /> // define Plane
+        <Box /> // define Box
+      </Physics>
     </Canvas>
   );
 }
